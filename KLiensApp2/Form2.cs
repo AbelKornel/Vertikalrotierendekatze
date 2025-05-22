1ee4cdd2be8bfeb4ec827de561011b0cf5825d25
rttjnk <janka.rott@stud.uni-corvinus.hu>
2025-05-22 11:14:23 +0200

    sikreses adatkötések

--- KLiensApp2/Form2.cs
+++ KLiensApp2/Form2.cs
@@ -20,6 +20,14 @@ namespace KLiensApp2
             InitializeComponent();
 
             _context.hcc_SearchQuery.Load();
+            _context.ProductsToOrder.Load();
+            _context.OrderedProducts.Load();
+
+            comboBox1.DataSource = _context.hcc_SearchQuery.Local;
+            comboBox1.DisplayMember = "QueryPhrase";
+            listBox1.DataSource = _context.ProductsToOrder.Local;
+            listBox1.DisplayMember = "Phrase";
+            dataGridView1.DataSource = _context.OrderedProducts.Local;
         }
     }
 }
