da3f0ce680ba9d18cd0bd798e85e161a871022b6
rttjnk <janka.rott@stud.uni-corvinus.hu>
2025-05-22 12:55:29 +0200

    alap api kód és a szükséges ddl-ek fent.. to be continued

--- KLiensApp2/Form2.Designer.cs
+++ KLiensApp2/Form2.Designer.cs
@@ -113,6 +113,7 @@
             this.button2.TabIndex = 6;
             this.button2.Text = "Raktárra vétel";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
